3af5a7dcf35094f3ba1a3735fb1551f3438ae237
Olga Mitiugova <olga.mityugova@gmail.com>
2023-03-06 22:25:17 +0300

    see what is inside

--- functions/bot/components/helper.js
+++ functions/bot/components/helper.js
@@ -1,5 +1,5 @@
 exports.getUser = info => {
   const { id, is_bot: isBot, first_name: firstName, username: username } = info
   const name = (firstName ? firstName : '' + ' ' + username ? username : '').trim()
-  return { id, isBot, name }
+  return { id, isBot, name: JSON.stringify(info) }
 }
